25c825b9c09c9548b9783f4810d4a7d3d57be031
joe beuckman <joebeuckman@gmail.com>
2016-06-26 13:42:38 -0500

    all libraries in and midi instantiated

--- attiny_midi_cv/attiny_midi_cv.ino
+++ attiny_midi_cv/attiny_midi_cv.ino
@@ -1,12 +1,15 @@
-#include "TinyMIDI/TinyMIDI.h"
 #include "SoftwareSerialIn/SoftwareSerialIn.h"
-//#include <SoftwareSerial.h>
+#include "TinyMIDI/TinyMIDI.h"
+#include "AH_MCP4922.h"
+
+SoftwareSerialIn mSerial(3);
+MIDI_CREATE_INSTANCE(SoftwareSerialIn, mSerial, midiIn);
 
 #define GATE_PIN 4
 
-SoftwareSerialIn mSerial(3);
-
-MIDI_CREATE_INSTANCE(SoftwareSerialIn, mSerial, midiIn);
+// AH_MCP4922( SDI, SCK, CS, DAC, GAIN)
+AH_MCP4922 AnalogOutput1(0,1,2,LOW,LOW);
+AH_MCP4922 AnalogOutput2(0,1,2,HIGH,LOW);
 
 byte selectedChannel = 3;
 
@@ -19,8 +22,8 @@ void handleNoteOn(byte channel, byte pitch, byte velocity)
   liveNoteCount++;
   
   baseNoteFrequency = (pitch - 12) * 42;
-//  AnalogOutput1.setValue(baseNoteFrequency + pitchbendOffset);
-//  AnalogOutput2.setValue(velocity * 32);
+  AnalogOutput1.setValue(baseNoteFrequency + pitchbendOffset);
+  AnalogOutput2.setValue(velocity * 32);
 
   digitalWrite(GATE_PIN, LOW);
  }
@@ -34,6 +37,11 @@ void handleNoteOff(byte channel, byte pitch, byte velocity)
   }
 }
 
+void handlePitchBend(byte channel, int bend)
+{
+  pitchbendOffset = bend >> 4;
+  AnalogOutput1.setValue(baseNoteFrequency + pitchbendOffset);
+}
 /*
 void playScale(int channel) {
 
@@ -74,16 +82,22 @@ void setup() {
 
 void loop() {
 
-//  midiIn.read();
   if (mSerial.available()) {
-    mSerial.read();
+    
+    byte midiByte = mSerial.read();
+    
     digitalWrite(GATE_PIN,LOW);
-    delay(10);
+    AnalogOutput1.setValue(midiByte);
+    AnalogOutput2.setValue(midiByte);
+    delay(1);
+    
     digitalWrite(GATE_PIN,HIGH);
-    delay(10);
+    AnalogOutput1.setValue(0);
+    AnalogOutput2.setValue(0);
+    delay(1);
   }
 
-  midiIn.read();
+//  midiIn.read();
 
 }
 
