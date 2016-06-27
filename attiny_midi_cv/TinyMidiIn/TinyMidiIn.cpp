8280458a9c058ce6002bf15cbd83c79ec5bb4328
joe beuckman <joebeuckman@gmail.com>
2016-06-26 22:27:59 -0500

    basic note on off working

--- attiny_midi_cv/TinyMidiIn/TinyMidiIn.cpp
+++ attiny_midi_cv/TinyMidiIn/TinyMidiIn.cpp
@@ -8,14 +8,99 @@ TinyMidiIn::TinyMidiIn()
     mPitchBendCallback              = 0;
     mSystemExclusiveCallback        = 0;
     mSystemResetCallback            = 0;
+    
+    messagePosition = 0;
+}
+
+void TinyMidiIn::setFilterChannel(byte channel)
+{
+    filterChannel = channel;
 }
 
 void TinyMidiIn::inputByte(byte midiByte)
 {
-    if (mNoteOnCallback != 0) {
-        mNoteOnCallback(1, 60, 100);
+    if (messagePosition == 0) {
+        
+        nib1 = midiByte & B11110000;
+        nib2 = midiByte & B00001111;
+        
+        switch (nib1) {
+                
+            case 0xF0:
+                break;
+                
+            case NoteOn:
+            case NoteOff:
+            case PitchBend:
+                messageType = nib1;
+                messagePosition++;
+                break;
+                
+            default:
+                messageType = 0;
+                break;
+        }
+
+    } 
+    else if (messagePosition == 1) {
+        
+        switch (messageType) {
+            case NoteOn:
+            case NoteOff:
+            case PitchBend:
+                data1 = midiByte;
+                messagePosition++;
+                break;
+                
+            default:
+                messagePosition = 0;
+                break;
+        }
+    }
+    else if (messagePosition == 2) {
+        
+        switch (messageType) {
+            case NoteOn:
+            case NoteOff:
+            case PitchBend:
+                data2 = midiByte;
+                executeChannelMessage();
+                messagePosition = 0;
+                break;
+                
+            default:
+                messagePosition = 0;
+                break;
+        }
+    }
+    
+}
+
+
+void TinyMidiIn::executeChannelMessage()
+{
+    switch (messageType) {
+
+        case NoteOn:
+            if (mNoteOnCallback != 0) {
+                mNoteOnCallback(filterChannel, data1, data2);
+            }
+            break;
+            
+        case NoteOff:
+            if (mNoteOffCallback != 0) {
+                mNoteOffCallback(filterChannel, data1, data2);
+            }
+            break;
+            
+        case PitchBend:
+            if (mPitchBendCallback != 0) {
+                mPitchBendCallback(filterChannel, ((int)data1 << 8) + (int)data2);
+            }
+            break;
     }
 }
+
 
 void TinyMidiIn::setHandleNoteOff(void (*fptr)(byte channel, byte note, byte velocity)) { mNoteOffCallback = fptr; }
 void TinyMidiIn::setHandleNoteOn(void (*fptr)(byte channel, byte note, byte velocity)) { mNoteOnCallback = fptr; }
--- attiny_midi_cv/TinyMidiIn/TinyMidiIn.h
+++ attiny_midi_cv/TinyMidiIn/TinyMidiIn.h
@@ -3,26 +3,51 @@
 
 #include "Arduino.h"
 
+/*! Enumeration of MIDI types */
+enum MidiType
+{
+    InvalidType           = 0x00,    ///< For notifying errors
+    NoteOff               = 0x80,    ///< Note Off
+    NoteOn                = 0x90,    ///< Note On
+    PitchBend             = 0xE0,    ///< Pitch Bend
+    NotesOff              = 0xA0,    ///< AllNotesOff
+    SystemExclusive       = 0xF0,    ///< System Exclusive
+    SystemReset           = 0xFF,    ///< System Real Time - System Reset
+};
+
 
 class TinyMidiIn {
 
-public:
-    TinyMidiIn();
+    public:
+        TinyMidiIn();
 
-    void inputByte(byte midiByte);
+        void inputByte(byte midiByte);
+    
+        void setFilterChannel(byte channel);
 
-    void setHandleNoteOff(void (*fptr)(byte channel, byte note, byte velocity));
-    void setHandleNoteOn(void (*fptr)(byte channel, byte note, byte velocity));
-    void setHandlePitchBend(void (*fptr)(byte channel, int bend));
-    void setHandleSystemExclusive(void (*fptr)(byte * array, unsigned size));
-    void setHandleSystemReset(void (*fptr)(void));
+        void setHandleNoteOff(void (*fptr)(byte channel, byte note, byte velocity));
+        void setHandleNoteOn(void (*fptr)(byte channel, byte note, byte velocity));
+        void setHandlePitchBend(void (*fptr)(byte channel, int bend));
+        void setHandleSystemExclusive(void (*fptr)(byte * array, unsigned size));
+        void setHandleSystemReset(void (*fptr)(void));
 
     private:
+    
+        byte nib1, nib2;
+        byte filterChannel;
+
+        byte messageType;
+        byte messagePosition;
+        byte data1;
+        byte data2;
+    
+        void executeChannelMessage();
+    
         void (*mNoteOffCallback)(byte channel, byte note, byte velocity);
-    void (*mNoteOnCallback)(byte channel, byte note, byte velocity);
-    void (*mPitchBendCallback)(byte channel, int);
-    void (*mSystemExclusiveCallback)(byte * array, unsigned size);
-    void (*mSystemResetCallback)(void);
+        void (*mNoteOnCallback)(byte channel, byte note, byte velocity);
+        void (*mPitchBendCallback)(byte channel, int);
+        void (*mSystemExclusiveCallback)(byte * array, unsigned size);
+        void (*mSystemResetCallback)(void);
 };
 
 #endif
